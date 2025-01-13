0a3925a6cbd68636dc413e7ddd65d4b83db87c4d
Tim Kelly <timlkelly@users.noreply.github.com>
2025-01-13 17:54:19 -0600

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+GENRES = [
+  'action',
+  'thriller',
+  'comedy',
+  'horror',
+  'romance',
+  'western',
+  'science fiction'
+].freeze
+
+10.times do
+  User.create(
+    name: Faker::Name.name,
+    phone_number: Faker::PhoneNumber.unique.phone_number
+  )
+end
+
+50.times { Movie.create(title: Faker::Movie.unique.title, genre: GENRES.sample) }
+
+5.times do
+  Rental.create(user_id: User.all.sample.id, movie_id: Movie.all.sample.id)
+end
